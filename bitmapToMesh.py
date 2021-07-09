811e5a6f9c7fd574ad2d3d4f7429815c5b0c87b1
David <ddleon@outlook.co.il>
2021-07-09 23:38:13 +0300

    Update bitmapToMesh.py

--- bitmapToMesh.py
+++ bitmapToMesh.py
@@ -4,48 +4,36 @@ import cv2
 FILENAME = "Gaussian-Image-of-the-point-cloud-represented-in-Fig-3.png"
 
 img = cv2.imread(FILENAME)
-grayImg = 255 - cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+grayImg = 255 - cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-imgContours, imgHierarchy = cv2.findContours(grayImg,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-imgRect = cv2.rectangle(grayImg,(0,0),(grayImg.shape[0]-1,grayImg.shape[1]-1),(255,0,0))
-subdiv = cv2.Subdiv2D((0,0,grayImg.shape[0]-1,grayImg.shape[1]-1))
+subdiv = cv2.Subdiv2D((0, 0, grayImg.shape[0]-1, grayImg.shape[1]-1))
 
 vertexArray = []
-
-cv2.Subdiv2D()
-#cv2.imshow("image",grayImg)
-#cv2.waitKey(50000)
-
 vertexArrayIndexHash = dict()
 
-for x in range(0,grayImg.shape[0]-1):
-    for y in range(0,grayImg.shape[1]-1):
-        if grayImg[x,y] > 0:
-            vertexArray.append((x,y,grayImg[x,y]))
-            vertexArrayIndexHash[(x,y)] = len(vertexArray)-1
-            subdiv.insert((x,y))
+for x in range(0, grayImg.shape[0]-1):
+    for y in range(0, grayImg.shape[1]-1):
+        if grayImg[x, y] > 0:
+            vertexArray.append((x, y, grayImg[x,y]))
+            vertexArrayIndexHash[(x, y)] = len(vertexArray)-1
+            subdiv.insert((x, y))
 
 facesList = subdiv.getTriangleList()
-
 facesArray = []
 
 checklistArray = [0]*len(vertexArray)
 
 for face in facesList:
     newFaceTuple = []
-    for i in range(0,6,2):
+    for i in range(0, 6, 2):
         x = face[i]
         y = face[i+1]
-        newFaceTuple.append(vertexArrayIndexHash[(x,y)])
-        checklistArray[vertexArrayIndexHash[(x,y)]] = 1
+        newFaceTuple.append(vertexArrayIndexHash[(x, y)])
+        checklistArray[vertexArrayIndexHash[(x, y)]] = 1
+
     facesArray.append(tuple(newFaceTuple))
 
 # validate that there is no vertex that is not being used in 2
-for idx,vertex in enumerate(checklistArray):
+for idx, vertex in enumerate(checklistArray):
     if vertex == 0:
         print("Error "+str(idx))
-
-cv2.Subdiv2D()
-cv2.imshow("image",grayImg)
-cv2.waitKey(5000)
